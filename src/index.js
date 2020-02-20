f75f14561a008beaf3abd33591c8334b22c83eae
AnturK <AnturK@users.noreply.github.com>
2020-02-20 12:14:45 +0100

    Initial version.

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+    const client = new github.GitHub(token);
+    const issue_number = getIssueNumber();
+    if (!issue_number) {
+      core.setFailed("Issue number retrieval failed");
+      return;
+    }
+    const issue_body = await getIssueBody(client, issue_number)
+    if (!issue_body) {
+      core.setFailed("Issue body retrieval failed");
+      return;
+    }
+    createLinks(client, issue_number, issue_body)
+  }
+  catch (e) {
+    core.setFailed("Action failed.");
+  }
+
+}
+
+function getIssueNumber() {
+  const issue = github.context.payload.issue;
+  if (!issue) {
+    return undefined;
+  }
+  return issue.number;
+}
+
+async function getIssueBody(client, issue_number) {
+  const getResponse = await client.issues.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issue_number
+  });
+  return getResponse.body
+}
+
+// Would be less intrusive but more spammy with a comment, undecided.
+async function createLinks(client, issue_number, issue_body) {
+  const new_body = issue_body.replace(/(\[Round ID\]: )(\d+)/g, "$1[$2](https://scrubby.melonmesa.com/round/$2)");
+
+  const getResponse = await client.issues.update({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issue_number,
+    body: new_body
+  });
+}
+
+run();
