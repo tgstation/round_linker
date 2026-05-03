68a64380f0aaa8a1dd011d99b233186130e60cff
SyncIt21 <VLord3D@gmail.com>
2026-05-03 19:32:31 +0530

    octokit fixes & function merges

--- dist/index.js
+++ dist/index.js
@@ -33058,56 +33058,45 @@ function getOctokit(token, options, ...additionalPlugins) {
 
 async function run() {
   try {
-    const token = getInput("repo-token", { required: true });
-    const issue_number = getIssueNumber();
+    // Get issue number of the payload
+    const issue_number = context.payload.issue?.number;
     if (!issue_number) {
       setFailed("Issue number retrieval failed");
       return;
     }
-    const client = new getOctokit(token);
-    const issue_body = await getIssueBody(client, issue_number);
+
+    //github client to make requests
+    const octokit = new getOctokit(getInput("repo-token", { required: true }));
+
+    //get issue body
+    const data = await octokit.rest.issues.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: issue_number,
+    });
+    const issue_body = data.body;
     if (!issue_body) {
       setFailed("Issue body retrieval failed");
       return;
     }
-    createLinks(client, issue_number, issue_body);
+
+    //modify issue body with round link id
+    const re = /(\[?Round ID\]?:\s*)(\d+)/g;
+    if (issue_body.match(re)) {
+      const new_body = issue_body.replace(
+        re,
+        "$1[$2](https://statbus.space/round/$2)",
+      );
+
+      await octokit.rest.issues.update({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issue_number,
+        body: new_body,
+      });
+    }
   } catch (e) {
     setFailed("Action failed.");
-  }
-}
-
-function getIssueNumber() {
-  const issue = context.payload.issue;
-  if (!issue) {
-    return undefined;
-  }
-  return issue.number;
-}
-
-async function getIssueBody(client, issue_number) {
-  const getResponse = await client.issues.get({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: issue_number,
-  });
-  return getResponse.data.body;
-}
-
-// Would be less intrusive but more spammy with a comment, undecided.
-async function createLinks(client, issue_number, issue_body) {
-  let re = /(\[?Round ID\]?:\s*)(\d+)/g;
-  if (issue_body.match(re)) {
-    const new_body = issue_body.replace(
-      re,
-      "$1[$2](https://statbus.space/round/$2)",
-    );
-
-    await client.issues.update({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: issue_number,
-      body: new_body,
-    });
   }
 }
 
