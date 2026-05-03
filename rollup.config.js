4a1509b0ed77e6aae8626840ad295fd741a2655b
SyncIt21 <VLord3D@gmail.com>
2026-05-03 22:30:53 +0530

    rename

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/index.js",
+    format: "es",
+  },
+  plugins: [commonjs(), nodeResolve()],
+};
