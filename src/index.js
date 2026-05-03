71fa2609cc06fac23a723851e1f37d092b3af649
SyncIt21 <110812394+SyncIt21@users.noreply.github.com>
2026-05-03 19:57:57 +0530

    Update everything to latest (#16)
    * Switches bundler to rollupand updates depdencies
    * Adds CI and prettier

--- src/index.js
+++ src/index.js
@@ -1,58 +1,47 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setFailed } from "@actions/core";
+import { getOctokit, context } from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput("repo-token", { required: true });
-    const issue_number = getIssueNumber();
+    // Get issue number of the payload
+    const issue_number = context.payload.issue?.number;
     if (!issue_number) {
-      core.setFailed("Issue number retrieval failed");
+      setFailed("Issue number retrieval failed");
       return;
     }
-    const client = new github.GitHub(token);
-    const issue_body = await getIssueBody(client, issue_number)
-    if (!issue_body) {
-      core.setFailed("Issue body retrieval failed");
-      return;
-    }
-    createLinks(client, issue_number, issue_body)
-  }
-  catch (e) {
-    core.setFailed("Action failed.");
-  }
 
-}
+    //github client to make requests
+    const octokit = new getOctokit(getInput("repo-token", { required: true }));
 
-function getIssueNumber() {
-  const issue = github.context.payload.issue;
-  if (!issue) {
-    return undefined;
-  }
-  return issue.number;
-}
-
-async function getIssueBody(client, issue_number) {
-  const getResponse = await client.issues.get({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue_number
-  });
-  return getResponse.data.body
-}
-
-// Would be less intrusive but more spammy with a comment, undecided.
-async function createLinks(client, issue_number, issue_body) {
-  let re = /(\[?Round ID\]?:\s*)(\d+)/g
-  if(issue_body.match(re))
-  {
-    const new_body = issue_body.replace(re, "$1[$2](https://statbus.space/round/$2)");
-
-    const getResponse = await client.issues.update({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
+    //get issue body
+    const data = await octokit.rest.issues.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
       issue_number: issue_number,
-      body: new_body
     });
+    const issue_body = data.body;
+    if (!issue_body) {
+      setFailed("Issue body retrieval failed");
+      return;
+    }
+
+    //modify issue body with round link id
+    const re = /(\[?Round ID\]?:\s*)(\d+)/g;
+    if (issue_body.match(re)) {
+      const new_body = issue_body.replace(
+        re,
+        "$1[$2](https://statbus.space/round/$2)",
+      );
+
+      await octokit.rest.issues.update({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issue_number,
+        body: new_body,
+      });
+    }
+  } catch (e) {
+    setFailed(`Action failed ${e}.`);
   }
 }
 
