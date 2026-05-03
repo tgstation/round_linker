46dc73ce3f24cfc24effe62651c0113a4625a55c
SyncIt21 <VLord3D@gmail.com>
2026-05-03 16:27:25 +0530

    Update everything to latest

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setFailed } from "@actions/core";
+import { getOctokit, context } from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput("repo-token", { required: true });
+    const token = getInput("repo-token", { required: true });
     const issue_number = getIssueNumber();
     if (!issue_number) {
-      core.setFailed("Issue number retrieval failed");
+      setFailed("Issue number retrieval failed");
       return;
     }
-    const client = new github.GitHub(token);
-    const issue_body = await getIssueBody(client, issue_number)
+    const client = new getOctokit(token);
+    const issue_body = await getIssueBody(client, issue_number);
     if (!issue_body) {
-      core.setFailed("Issue body retrieval failed");
+      setFailed("Issue body retrieval failed");
       return;
     }
-    createLinks(client, issue_number, issue_body)
+    createLinks(client, issue_number, issue_body);
+  } catch (e) {
+    setFailed("Action failed.");
   }
-  catch (e) {
-    core.setFailed("Action failed.");
-  }
-
 }
 
 function getIssueNumber() {
-  const issue = github.context.payload.issue;
+  const issue = context.payload.issue;
   if (!issue) {
     return undefined;
   }
@@ -33,25 +31,27 @@ function getIssueNumber() {
 
 async function getIssueBody(client, issue_number) {
   const getResponse = await client.issues.get({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue_number
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issue_number,
   });
-  return getResponse.data.body
+  return getResponse.data.body;
 }
 
 // Would be less intrusive but more spammy with a comment, undecided.
 async function createLinks(client, issue_number, issue_body) {
-  let re = /(\[?Round ID\]?:\s*)(\d+)/g
-  if(issue_body.match(re))
-  {
-    const new_body = issue_body.replace(re, "$1[$2](https://statbus.space/round/$2)");
+  let re = /(\[?Round ID\]?:\s*)(\d+)/g;
+  if (issue_body.match(re)) {
+    const new_body = issue_body.replace(
+      re,
+      "$1[$2](https://statbus.space/round/$2)",
+    );
 
     const getResponse = await client.issues.update({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
       issue_number: issue_number,
-      body: new_body
+      body: new_body,
     });
   }
 }
