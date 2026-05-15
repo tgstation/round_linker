9ea2b1800da65eb7fb06ec4c4daf407299a27ba8
SyncIt21 <VLord3D@gmail.com>
2026-05-15 21:34:50 +0530

    A Faster Way

--- dist/index.js
+++ dist/index.js
@@ -33060,32 +33060,32 @@ async function run() {
     try {
         // Get issue number of the payload
         const issue_number = context.payload.issue?.number;
-        if (!issue_number) {
+        if (issue_number == undefined) {
             setFailed('Issue number retrieval failed');
             return;
         }
-        //github client to make requests
-        const octokit = getOctokit(getInput('repo-token', { required: true }));
         //get issue body
-        const response = await octokit.rest.issues.get({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: issue_number
-        });
-        const issue_body = response.data.body;
-        if (!issue_body) {
+        let issue_body = context.payload.issue?.body;
+        if (issue_body == undefined) {
             setFailed('Issue body retrieval failed');
+            return;
+        }
+        //issue body was empty after trim
+        issue_body = issue_body.trim();
+        if (issue_body.length == 0) {
             return;
         }
         //modify issue body with round link id
         const re = /(\[?Round ID\]?:\s*)(\d+)/g;
         if (issue_body.match(re)) {
-            const new_body = issue_body.replace(re, '$1[$2](https://statbus.space/round/$2)');
+            issue_body = issue_body.replace(re, '$1[$2](https://statbus.space/round/$2)');
+            //github client to make requests
+            const octokit = getOctokit(getInput('repo-token', { required: true }));
             await octokit.rest.issues.update({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
                 issue_number: issue_number,
-                body: new_body,
+                body: issue_body,
                 headers: {
                     'X-GitHub-Api-Version': '2026-03-10'
                 }
