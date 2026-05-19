3f36c6888d4367a572314f3433c9ccbaff4a462d
SyncIt21 <VLord3D@gmail.com>
2026-05-19 16:18:21 +0530

    test changes

--- dist/index.js
+++ dist/index.js
@@ -33058,7 +33058,7 @@ function getOctokit(token, options, ...additionalPlugins) {
 
 async function run() {
     try {
-        // Get issue number of the payload
+        //get issue number of the payload
         const issue_number = context.payload.issue?.number;
         if (issue_number == undefined) {
             setFailed('Issue number retrieval failed');
@@ -33069,22 +33069,39 @@ async function run() {
         if (!issue_body) {
             return;
         }
+        //check for regex changes
+        let changes = false;
         //modify issue body with round link id
         const re = /(\[?Round ID\]?:\s*)(\d+)/g;
         if (issue_body.match(re)) {
             issue_body = issue_body.replace(re, '$1[$2](https://statbus.space/round/$2)');
-            //github client to make requests
-            const octokit = getOctokit(getInput('repo-token', { required: true }));
-            await octokit.rest.issues.update({
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                issue_number: issue_number,
-                body: issue_body,
-                headers: {
-                    'X-GitHub-Api-Version': '2026-03-10'
-                }
-            });
+            changes = true;
         }
+        //modify issue body with byond client download link
+        const ce = /(\[?Client Version\]?:\s*)((\d+)\.(\d+))/g;
+        if (issue_body.match(ce)) {
+            issue_body = issue_body.replace(ce, '$1' +
+                '[$2 Windows](https://www.byond.com/download/build/$3/$2_byond_setup.zip)' + //windows zip file with installer
+                '\n' +
+                '[$2 Linux](https://www.byond.com/download/build/$3/$2_byond_linux.zip)' //linux zip folder
+            );
+            changes = true;
+        }
+        //no changes
+        if (!changes) {
+            return;
+        }
+        //github client to make requests
+        const octokit = getOctokit(getInput('repo-token', { required: true }));
+        await octokit.rest.issues.update({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issue_number,
+            body: issue_body,
+            headers: {
+                'X-GitHub-Api-Version': '2026-03-10'
+            }
+        });
     }
     catch (e) {
         setFailed(`Action failed ${e}.`);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {getOctokit, context} from '@actions/github'
 
 async function run(): Promise<void> {
     try {
-        // Get issue number of the payload
+        //get issue number of the payload
         const issue_number = context.payload.issue?.number
         if (issue_number == undefined) {
             setFailed('Issue number retrieval failed')
@@ -16,6 +16,9 @@ async function run(): Promise<void> {
             return
         }
 
+        //check for regex changes
+        let changes = false
+
         //modify issue body with round link id
         const re = /(\[?Round ID\]?:\s*)(\d+)/g
         if (issue_body.match(re)) {
@@ -23,22 +26,41 @@ async function run(): Promise<void> {
                 re,
                 '$1[$2](https://statbus.space/round/$2)'
             )
-
-            //github client to make requests
-            const octokit: ReturnType<typeof getOctokit> = getOctokit(
-                getInput('repo-token', {required: true})
-            )
-
-            await octokit.rest.issues.update({
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                issue_number: issue_number,
-                body: issue_body,
-                headers: {
-                    'X-GitHub-Api-Version': '2026-03-10'
-                }
-            })
+            changes = true
         }
+
+        //modify issue body with byond client download link
+        const ce = /(\[?Client Version\]?:\s*)((\d+)\.(\d+))/g
+        if (issue_body.match(ce)) {
+            issue_body = issue_body.replace(
+                ce,
+                '$1' +
+                    '[$2 Windows](https://www.byond.com/download/build/$3/$2_byond_setup.zip)' + //windows zip file with installer
+                    '\n' +
+                    '[$2 Linux](https://www.byond.com/download/build/$3/$2_byond_linux.zip)' //linux zip folder
+            )
+            changes = true
+        }
+
+        //no changes
+        if (!changes) {
+            return
+        }
+
+        //github client to make requests
+        const octokit: ReturnType<typeof getOctokit> = getOctokit(
+            getInput('repo-token', {required: true})
+        )
+
+        await octokit.rest.issues.update({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issue_number,
+            body: issue_body,
+            headers: {
+                'X-GitHub-Api-Version': '2026-03-10'
+            }
+        })
     } catch (e) {
         setFailed(`Action failed ${e}.`)
     }
